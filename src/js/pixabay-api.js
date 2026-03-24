136dfef32e90f654b4c97c411b845be9f1307309
Dmitro-1992 <belyj2336@gmail.com>
2026-03-24 18:10:04 +0200

    pixabay-api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,25 @@
+import axios from "axios";
+
 const API_KEY = "29372998-77c4088f99339c8fc310ed4d5";
 const BASE_URL = "https://pixabay.com/api/";
 const PER_PAGE = 15;
 
 export async function getImagesByQuery(query, page = 1) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PER_PAGE}`;
-  
   try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
-    return await response.json();
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+        page,
+        per_page: PER_PAGE,
+      },
+    });
+
+    // Возвращаем именно data, как требует задание
+    return response.data;
   } catch (error) {
     console.error(error);
     throw error;
